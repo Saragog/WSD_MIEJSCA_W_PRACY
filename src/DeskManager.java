7e89c09ddd6e6f611c1ac194833c05d3196f69ea
Saragog <andrzejdackiewicz@gmail.com>
2017-12-03 14:34:31 +0100

    First Commit

---
+++ src/DeskManager.java
@@ -0,0 +1,17 @@
+/*
+ * This program is a project for subject WSD taken on Warsaw university of technology
+ * It is based on multi agent cooperation
+ * This class creates all the agents and starts the program
+ */
+
+import jade.core.Agent;
+
+public class DeskManager {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World !!!");
+		Agent a = new Agent();
+		a.addBehaviour(null);
+	}
+
+}
