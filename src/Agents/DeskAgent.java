8ad2dbd84d1d9ee20ee05c6e3e1c4b8c9685a8d4
Saragog <andrzejdackiewicz@gmail.com>
2017-12-10 16:33:40 +0100

    Dodana klasa desk agent

---
+++ src/Agents/DeskAgent.java
@@ -0,0 +1,10 @@
+package Agents;
+
+import jade.core.Agent;
+
+public class DeskAgent extends Agent {
+	protected void setup() {
+		System.out.println("Siema tutaj agent: " + getAID().getName()+" jestem gotowy!!!");
+	}
+
+}
