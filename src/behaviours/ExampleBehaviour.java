6e0c7670e771a21a2ce6aeecb373a0916829a600
Saragog <andrzejdackiewicz@gmail.com>
2017-12-11 22:32:54 +0100

    Added a behaviours package and first behaviour class - to be changed

---
+++ src/behaviours/ExampleBehaviour.java
@@ -0,0 +1,16 @@
+package behaviours;
+
+import jade.core.behaviours.CyclicBehaviour;
+
+public class ExampleBehaviour extends CyclicBehaviour{
+	
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void action() {
+		 // perform operation X
+		 }
+	 
+}
