446cca16022d809f642b3bbe49b0507cf22d749c
marek <M.Borkowski@stud.elka.pw.edu.pl>
2017-12-17 15:36:41 +0100

    Nadawanie preferencji oraz przekazywanie AID biurek do pracownikow.

--- src/boot/MobileDeskReservationBoot.java
+++ src/boot/MobileDeskReservationBoot.java
@@ -17,29 +17,76 @@ public class MobileDeskReservationBoot {
 	    profile.setParameter(Profile.MAIN_HOST, "localhost");
 	    profile.setParameter(Profile.GUI, "true");
 	    ContainerController containerController = runtime.createMainContainer(profile);
-	    Object[] employeeArgs = new Object[2];
-	    AID[] allDesks = new AID[2];
-	    int[] preferredDesksIndices = new int[4];
+	    Object[] employeeArgs = new Object[2];	//liczba argumentow odbieranych przez pracownika
+	    AID[] allDesks = new AID[4];	// identyfikatory biurek 
+	    int[] preferredDesksIndices = new int[4];	// identyfikatory biurek preferowanych przez pracownika
 	    //wypelnic 
 	    employeeArgs[0] = allDesks;
 	    employeeArgs[1] = preferredDesksIndices;
 	    
-  
-	    for(int i=1; i<2; i++)
+	    initpreferredDesksIndices(1, preferredDesksIndices);
+	    
+	    /*-------------------------
+	     * Tworzenie agentow biurek
+	     * ------------------------*/
+	    for(int i=1; i<=4; i++)
 	    {
 	    	AgentController deskAgentController;
-	    	AgentController employeeAgentController;
 	        try
 	        {
 	        	deskAgentController = containerController.createNewAgent("Biurko"+i, "agents.DeskAgent", null);
-	            deskAgentController.start();    
-	            employeeAgentController = containerController.createNewAgent("Pracownik" + i, "agents.EmployeeAgent", employeeArgs);
+	            deskAgentController.start();
+	            allDesks[i-1] = new AID("Biurko"+i,AID.ISLOCALNAME);
+	        }
+	        catch (StaleProxyException e)
+	        {
+	        	e.printStackTrace();
+	        }
+	    }
+	    
+	    /*-----------------------------
+	     * Tworzenie agentow praciownikow.
+	     *------------------------------*/
+	    for(int i=1; i<=2; i++)
+	    {
+	    	AgentController employeeAgentController;
+	        try
+	        {
+	        	initpreferredDesksIndices(i, preferredDesksIndices); // nadawanie preferencji pracownikom
+	        	employeeAgentController = containerController.createNewAgent("Pracownik" + i, "agents.EmployeeAgent", employeeArgs);
 	            employeeAgentController.start();
 	        }
 	        catch (StaleProxyException e)
 	        {
 	        	e.printStackTrace();
 	        }
-	    }	
+	    }
+	    
 	}
+
+	/*-------------------------
+	 * Preferencje pracownikow.
+	 * -----------------------*/
+	private static void initpreferredDesksIndices(int option, int[] preferredDesksIndices) {
+		switch (option) {
+			case 1: 
+			{
+				preferredDesksIndices[0] = 1;
+				preferredDesksIndices[1] = 2;
+				preferredDesksIndices[2] = 3;
+				preferredDesksIndices[3] = 4;
+				break;
+			}
+			case 2:
+			{
+				preferredDesksIndices[0] = 2;
+				preferredDesksIndices[1] = 1;
+				preferredDesksIndices[2] = 3;
+				preferredDesksIndices[3] = 4;
+				break;
+			}
+			
+		}
+	}
+	
 }
