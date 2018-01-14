6504ebfb104f87585024abd1f704023d6b7ddc25
Mat <Mat@MAT123456>
2018-01-14 17:23:33 +0100

    break

--- src/behaviours/DeskBehaviour.java
+++ src/behaviours/DeskBehaviour.java
@@ -89,7 +89,8 @@ public class DeskBehaviour extends CyclicBehaviour{
 					switch (agentState){
 					
 					/*5.1 Aukcja w stanie FREE*/
-						case FREE:{
+						case FREE:
+						{
 								Price price = getPrice(content);
 								System.out.println("FREE "+myAgent.getLocalName() + " otrzymano‚ propozycje:  " +price.tokens +", " + price.epsilons + " od " + msg.getSender().getLocalName());
 								//boolean win = auction(myDeskAgent, sender, price);
